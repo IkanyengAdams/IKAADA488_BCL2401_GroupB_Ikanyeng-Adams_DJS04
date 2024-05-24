ef238a876c2ff8dd945627caa6add513746414c5
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-05-24 16:31:34 +0200

    add comments to clarify my code

--- actions.js
+++ actions.js
@@ -1,14 +1,18 @@
 // Action creators
+
+//Function to add 1.
 export const add = (state) => ({
   ...state,
   count: state.count + 1
 });
 
+//Function to subtract 1.
 export const subtract = (state) => ({
   ...state,
   count: state.count - 1
 });
 
+//Function to reset Tally app
 export const reset = (state) => ({
   ...state,
   count: 0
