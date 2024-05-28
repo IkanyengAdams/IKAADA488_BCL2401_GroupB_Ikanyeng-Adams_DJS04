365cc5b8db30a26d9ce3eac6e513a80986ced3a2
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-05-28 13:59:39 +0200

    added comments on the actions.js file

--- actions.js
+++ actions.js
@@ -1,18 +1,21 @@
 // Action creators
 
-//Function to add 1.
+/*Function to add 1.
+*amplify one to the tally store*/
 export const add = (state) => ({
   ...state,
   count: state.count + 1
 });
 
-//Function to subtract 1.
+/*Function to subtract 1.
+*Deduct 1 from Tally store*/
 export const subtract = (state) => ({
   ...state,
   count: state.count - 1
 });
 
-//Function to reset Tally app
+/*Function to reset Tally app
+*reset state*/
 export const reset = (state) => ({
   ...state,
   count: 0
